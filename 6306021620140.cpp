2fc5c339b19ee66a03e1569f0d4708fad9b079b9
ThatsaneeSeeson <68506599+ThatsaneeSeeson@users.noreply.github.com>
2020-07-19 21:25:20 +0700

    Add files via upload

---
+++ 6306021620140.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "\n" << endl;
+	cout << "1st Term of 2020          \\45132114  Wanthanee Prachuabsupakij" << endl;
+	cout << "\n" << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "| Subject ID | Subject name             | Score (100) |  Grad |" << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "|  4570034   | Intriduction to computer |     85      |  \"A\"  |" << endl;
+	cout << "|  4580247   | English                  |     74      |  \"B\"  |" << endl;
+	cout << "|  4530001   | Design Thinking          |     92      |  \"A\"  |" << endl;
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "|   TOTAL    | Subject = 3              | Average = 3.87 |" << endl;
+	cout << "=====================================" << endl;
+	cout << "\n" << endl;
+	cout << "KMUTNB" << endl;
+	return (0);
+}
